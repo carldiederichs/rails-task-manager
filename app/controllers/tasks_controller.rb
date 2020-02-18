075be534f761db4fc90d990e23b881c949183e1a
Carl Diederichs <carl-diederichs@t-online.de>
2020-02-18 16:56:16 +0000

    Second challenge not completed

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,31 @@
+# See all tasks
+# See details about one task
+# Create a task
+# Update a task
+# Destroy a task
+
+class TasksController < ApplicationController
+
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to task_path(@task)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+end
